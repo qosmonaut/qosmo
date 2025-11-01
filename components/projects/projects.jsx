c11c5ade410b10317d608cea72ade28f72018742
Alen Roady <alenroady@gmail.com>
2025-11-01 01:55:31 +0100

    style: Dynamic column number in project section
    - Adjusted project section to support dynamic column numbers
    - Improved layout for better responsiveness

--- components/projects/projects.jsx
+++ components/projects/projects.jsx
@@ -5,7 +5,7 @@ const Projects = (props) => {
     return (
         <div data-section id='projects' className='mb-16'>
             <h2 className='mb-8 visible lg:invisible font-medium tracking-widest'>Projects</h2>
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2'>
                 {props.data.map(function(object, index){
                     return <ProjectItem
                         key={object.name}
