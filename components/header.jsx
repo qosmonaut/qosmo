879e03123d91dc36f86cca50e9ca75c845dd278b
Alen Roady <alenroady@gmail.com>
2025-11-01 02:04:58 +0100

    style: Center header and socials on small viewport
    - Adjusted CSS classes to ensure header and social icons are centered
      on smaller screens for better visual alignment.
    - Switch from 'justify-between' to 'justify-center' in flexbox layout.
    - This enhances user experience on mobile devices and tablets.
    - Switch from double to single column layout for viewports < 1024px.

--- components/header.jsx
+++ components/header.jsx
@@ -4,11 +4,11 @@ import Socials from "./socials";
 
 const Header = (props) => {
     return (
-        <div className='lg:fixed h-screen basis-1.5/4 flex flex-col justify-between pb-48 self-center lg:self-auto'>
-            <div className=''>
+        <div className='lg:fixed h-screen basis-1.5/4 flex flex-col items-center text-center justify-center gap-8 pb-24 self-center lg:self-auto lg:items-start lg:text-left lg:justify-between lg:gap-0 lg:pb-48'>
+            <div className='flex flex-col items-center lg:items-start'>
                 <h1 className='text-5xl subpixel-antialiased tracking-wide'>{props.data.name}</h1>
                 <h2 className='text-surface-600 pt-2 text-base font-normal tracking-wider'>{props.data.headline}</h2>
-                <img src={props.data.image} alt={props.data.alt} className="mt-4 mb-6 rounded-lg shadow-lg" width="250"/>
+                <img src={props.data.image} alt={props.data.alt} className="mt-4 mb-6 rounded-lg shadow-lg mx-auto lg:mx-0" width="250"/>
                 <a href={props.data.cv} className="m-10 transition-all bg-primary-500 hover:bg-primary-300 text-surface-100 font-bold py-2 px-8 rounded">Download CV</a>
             </div>
             <Navigation></Navigation>
