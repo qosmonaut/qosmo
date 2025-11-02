b8c70de69b40870dec4a65f30131db3019812c45
Alen Roady <alenroady@gmail.com>
2025-11-02 20:26:58 +0100

    style: Adjust social links styling and icons
    - Replace icons with SVG versions for better scalability
    - Add hover effect for icons
    - Move CV download from sidebar to social links
    - Remove text for each service name

--- components/socials.jsx
+++ components/socials.jsx
@@ -3,24 +3,22 @@ import Image from 'next/image';
 
 const Socials = (props) => {
     return (
-        <div className='flex flex-row items-center justify-center lg:justify-start'>
-            <a href={`mailto:${props.data.email}`} className='mr-10 transition-all bg-primary-500 hover:bg-primary-300 text-surface-100 font-bold py-2 px-8 rounded flex flex-row justify-center items-center'>
+        <div className='flex flex-row items-center justify-center gap-8 lg:justify-start'>
+            <a href={`mailto:${props.data.email}`} className='bg-primary-300 hover:bg-primary-200 text-primary-50 flex flex-row items-center justify-center rounded px-4 py-2 font-bold transition-all'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                 </svg>
                 <span className='pl-2'>Contact</span>
             </a>
-            <div className='flex flex-row'>
-                <a href={props.data.github} target="_blank" rel="noopener noreferrer" className="mr-10 flex flex-row content-center justify-center">
-                    <Image src="/logos/github.png" alt="GitHub Logo" width={20} height={20} className='object-contain object-top mr-2' />
-                    <div className=''>GitHub</div>
-                </a>
-                <a href={props.data.linkedin} target="_blank" rel="noopener noreferrer" className="flex flex-row content-center justify-center">
-                    <Image src="/logos/linkedin.png" alt="LinkedIn Logo" width={20} height={20} className='object-contain object-top mr-2' />
-                    <div className=''>LinkedIn</div>
-                </a>
-            </div>
-            
+            <a href={props.data.github} target="_blank" rel="noopener noreferrer" className='transition-all hover:-translate-y-1'>
+                <Image src="/logos/github.svg" alt="GitHub Logo" width={20} height={20} className='object-contain object-top' />
+            </a>
+            <a href={props.data.linkedin} target="_blank" rel="noopener noreferrer" className='transition-all hover:-translate-y-1'>
+                <Image src="/logos/linkedin.svg" alt="LinkedIn Logo" width={20} height={20} className='object-contain object-top' />
+            </a>
+            <a href={props.data.cv} target="_blank" rel="noopener noreferrer" className='transition-all hover:-translate-y-1'>
+                <Image src="/logos/cv.svg" alt="Download CV" width={20} height={20} className='object-contain object-top' />
+            </a>
         </div>
     )
 }
