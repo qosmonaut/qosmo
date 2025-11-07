a0a1951b74d1aa1df9ce1572bbd9763b29f409cf
Alen Roady <alenroady@gmail.com>
2025-11-07 14:41:23 +0100

    style: Consistent formatting of *-item components
    - Ensured consistent style across all *-item components
    - Added arrow animation for links in education items
    - Shotened education titles to BSc/MSc

--- components/education/education-item.jsx
+++ components/education/education-item.jsx
@@ -3,10 +3,15 @@ import React from "react";
 function EducationItem(props) {
     return (
         <div className="group bg-background hover:bg-primary-100 mb-4 flex flex-row p-5 transition-all">
-            <div className='text-primary-400 mr-2 basis-1/4 text-xs'>{props.startDate} – {checkEndDate(props)}</div>
+            <div className='text-primary-400 mr-2 basis-1/4 text-sm font-mono'>{props.startDate} – {checkEndDate(props)}</div>
             <div className='basis-3/4'>
-                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.subject} ({props.degree})</a>
-                <div className='text-primary-400 mb-4'>{props.university}</div>
+                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.degree} {props.subject}</a>
+                <a href={props.href} target="_blank" rel="noopener noreferrer" className='mb-4 flex flex-row items-center'>
+                    <span>{props.university}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="invisible ml-1 h-3 w-3 scale-50 transition ease-in-out group-hover:visible group-hover:scale-100">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                </a>
                 <div className='text-primary-400'>
                     {Array.isArray(props.description) ?
                         props.description.map((line, idx) => (
--- components/volunteering/volunteering-item.jsx
+++ components/volunteering/volunteering-item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function VolunteeringItem(props) {
     return (
         <div className="group bg-background hover:bg-primary-100 mb-4 flex flex-row p-5 transition-all">
-            <div className='text-primary-400 mr-2 basis-1/4 text-xs'>{props.startDate} – {checkEndDate(props)}</div>
+            <div className='text-primary-400 mr-2 basis-1/4 text-sm font-mono'>{props.startDate} – {checkEndDate(props)}</div>
             <div className='flex basis-3/4 flex-col'>
                 <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.position} at {props.event}</a>
                 <a href={props.href} target="_blank" rel="noopener noreferrer" className='text-primary-400 mb-4 flex flex-row items-center'>
