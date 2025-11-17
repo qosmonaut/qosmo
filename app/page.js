70af002552351689449f121e2011861df3cb5869
Alen Roady <alenroady@gmail.com>
2025-11-17 01:02:33 +0100

    refactor: Rename `sidebar` component to `profile`
    - The `sidebar` component has been renamed to `profile` for clarity.
    - Small code clean-ups and formatting adjustments.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from "@/components/sidebar";
+import Profile from "@/components/profile";
 import Navigation from "@/components/navigation";
 import Socials from "@/components/socials";
 import About from "@/components/about";
@@ -16,19 +16,17 @@ export default async function Home() {
 
   return (
     <main className="font-body flex min-h-screen flex-col items-center px-4 py-12 lg:px-24 lg:py-24">
-      <div className="z-2 flex w-full max-w-5xl flex-col text-base lg:flex-row">
-        <div className="flex flex-col gap-16 pb-32 lg:fixed lg:h-screen lg:justify-between">
-          <Sidebar data={data.general}></Sidebar>
-          <Navigation></Navigation>
-          <Socials data={data.general.socials}></Socials>
+          <Profile data={data.general} />
+          <Navigation data={data.navigation} />
+          <Socials data={data.general.socials} />
         </div>
         <div className='lg:pl-[35%]'>
-          <About data={data.general}></About>
-          <Experiences data={data.experiences}></Experiences>
-          <Education data={data.education}></Education>
-          <Volunteering data={data.volunteering}></Volunteering>
-          <Projects data={data.projects}></Projects>
-          <Credits data={data.general}></Credits>
+          <About data={data.general} />
+          <Experiences data={data.experiences} />
+          <Education data={data.education} />
+          <Volunteering data={data.volunteering} />
+          <Projects data={data.projects} />
+          <Credits data={data.general} />
         </div>
       </div>
     </main>
