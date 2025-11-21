52141c458c8b1f75e6e3d0a0f2ade5e2ded2652f
Alen Roady <alenroady@gmail.com>
2025-11-21 01:56:08 +0100

    chore: Adjust credits

--- components/credits.jsx
+++ components/credits.jsx
@@ -6,9 +6,9 @@ const Credits = (props) => {
         <div data-section id='credits' className='group mt-32'>
             <div className='text-primary-400'>
                 <div className='mb-18'>
-                    Built on a <a href="https://github.com/karlgrossmann/two-column-portfolio" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">template </a>
-                    by <a href="https://github.com/karlgrossmann/" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">Karl Grossmann </a>
-                    using <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">Next.js </a>
+                    Adapted from work by <a href="https://github.com/karlgrossmann/" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">Karl Grossmann </a>
+                    and <a href="https://github.com/bchiang7/" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">Brittany Chiang</a>.
+                    Built with <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all">Next.js </a>
                     and <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" className="hover:text-highlight transition-all"> Tailwind CSS</a>.
                 </div>
                 <div className='flex flex-row justify-between'>
