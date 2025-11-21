fb1866a64e50bfd41adb750e66a56db8e8be0cce
Alen Roady <alenroady@gmail.com>
2025-11-21 01:46:16 +0100

    content: Expand content and make style uniform
    - Added data-canvas project to projects section.
    - Expanded texts for volunteering experiences.
    - Adjusted link formatting and hover animation for experience items to
      match volunteering and education items.
    - Consistently used full names instead of abbreviations.

--- components/experiences/experience-item.jsx
+++ components/experiences/experience-item.jsx
@@ -5,7 +5,13 @@ function ExperienceItem(props) {
         <div className="group bg-background hover:bg-primary-100 mb-4 flex flex-row p-5 transition-all">
             <div className='text-primary-400 mr-2 basis-1/4 text-sm font-mono'>{props.startDate} – {props.endDate}</div>
             <div className='basis-3/4'>
-                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.title} | {props.company} </a>
+                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.title}</a>
+                <a href={props.href} target="_blank" rel="noopener noreferrer" className='mb-4 flex flex-row items-center'>
+                    <span>{props.company}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="invisible ml-1 h-3 w-3 scale-50 transition ease-in-out group-hover:visible group-hover:scale-100">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                </a>
                 <div className='text-primary-400 mb-4'>
                     {Array.isArray(props.description) ? 
                         props.description.map((line, idx) => (
